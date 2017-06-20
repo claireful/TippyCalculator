46a756f761ff6e27d3744e37f9d2858d0ebfc407
Claire Chen <cokeity@fb.com>
2017-06-20 13:27:40 -0700

    Initial Commit

---
+++ TippyCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TippyCalculator
+//
+//  Created by Claire Chen on 6/20/17.
+//  Copyright © 2017 Claire Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
